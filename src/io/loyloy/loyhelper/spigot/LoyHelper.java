ff433b8f43e1a2e1c1cbc9977f850946a03b8dad
Daniel Murray <daniel@nonit.me>
2016-04-22 17:02:12 +0100

    Added code

---
+++ src/io/loyloy/loyhelper/spigot/LoyHelper.java
@@ -0,0 +1,17 @@
+package io.loyloy.loyhelper.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LoyHelper extends JavaPlugin
+{
+    public LoyHelper()
+    {
+
+    }
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+}
